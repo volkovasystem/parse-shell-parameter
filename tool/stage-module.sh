b39632865b5a9c1974ce816ce950873eeed4676e
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2023-11-05 20:01:41 +0800

    Fixed npx.

--- tool/stage-module.sh
+++ tool/stage-module.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-npx --ignore-existing --yes @volkovasystem/format-package-json-file;
+[[ -f "$MODULE_ROOT_DIRECTORY_PATH/tool/transform-context-module.sh" ]] &&	\
+npm run transform-context-module;
+
+npx --ignore-existing @volkovasystem/format-package-json-file;
 
 npm run reset-module &&	\
 npm run setup-module &&	\
 npm run build-module &&	\
 npm run test-module &&	\
 npm run clean-module;
+
+[[ -f "$MODULE_ROOT_DIRECTORY_PATH/tool/reset-context-module.sh" ]] &&	\
+npm run reset-context-module;
